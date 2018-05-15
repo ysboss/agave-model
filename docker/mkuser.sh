4f1361aa7e66419d6183e30823aa1e50ccc540b9
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-05-15 10:30:28 -0500

    Update script to provide helpful error messages

--- docker/mkuser.sh
+++ docker/mkuser.sh
@@ -1,3 +1,13 @@
+if [ ! -d /home/jupuser ]
+then
+    echo "You need to mount the /home/jupuser directory. See instructions in the readme at https://github.com/ysboss/agave-model" >&2
+    exit 2
+fi
 UserID=$(ls -ld /home/jupuser|awk '{print $3}')
+if [ "$UserID" = root ]
+then
+    echo "You have attempted to mount /home/jupuser with either a non-existant directory or one owned by root." >&2
+    exit 3
+fi
 useradd -M -u $UserID jupuser
 sudo -u jupuser /runuser.sh
