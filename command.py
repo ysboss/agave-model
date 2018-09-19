39ab26ee40fc42ecccfda270cd5cbee8da85a47d
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-09-19 12:24:16 -0500

    Tweaking and making nicer.

--- command.py
+++ command.py
@@ -4,18 +4,20 @@ from subprocess import Popen, STDOUT, PIPE
 from shlex import split
 from setvar import repvar
 
-def cmd(arg,show=True,keep_endings=False):
+def cmd(arg,show=True,keep_endings=False,inputs=""):
     if type(arg) == str:
         pyargs = split(repvar(arg))
     else:
-        pyargs = arg
+        pyargs = [repvar(a) for a in arg]
     lines = []
     errs  = []
     if show:
         print("cmd:"," ".join(pyargs))
     else:
         print("cmd:",pyargs[0],"...")
-    with Popen(pyargs,stderr=PIPE,stdout=PIPE) as pipe:
+    with Popen(pyargs,stderr=PIPE,stdout=PIPE,stdin=PIPE,close_fds=True) as pipe:
+        pipe.stdin.write(inputs.encode())
+        pipe.stdin.close()
         for line in pipe.stdout.readlines():
             ld = line.decode()
             if not keep_endings:
