43acf51caf0ef8664d37fd59bfb55062c710372f
Steven Brandt <sbrandt@cct.lsu.edu>
2020-01-15 10:01:33 -0600

    Add missing file

---
+++ run_tab.py
@@ -0,0 +1,38 @@
+import input_params
+import logOp
+from command import cmd
+import re
+import os
+from jetlag_conf import get_uv
+
+def relink(dir_a, dir_b):
+    for f in os.listdir(dir_a):
+        fa = dir_a+"/"+f
+        fb = dir_b+"/"+f
+        if os.path.isdir(fa):
+            os.makedirs(fb, exist_ok=True)
+            relink(fa, fb)
+        else:
+            os.link(fa, fb)
+
+def run(b):
+    d = input_params.get('middleware')
+    m = input_params.get('machine_'+d)
+    p = input_params.get('NPROCS')
+    procs = []
+    for pv in re.findall(r'\d+',p):
+        procs += [int(pv)]
+    t = input_params.get('title').lower()
+    w = input_params.get('middleware')
+    j = input_params.get('jobname')
+    logOp.log('run:',m,p,t,w)
+    uv = get_uv()
+    with logOp.logOp:
+        cmd("rm -fr input.tgz run_dir")
+        cmd("mkdir -p run_dir")
+        with open("run_dir/runapp.sh","w") as fd:
+            print("singularity exec $SING_OPTS --pwd $PWD $IMAGE bash ./%s-app.sh" % t,file=fd)
+        cmd("cp %s-app.sh run_dir/" % t)
+        relink("input_%s" % t, "run_dir")
+        cmd("tar czvf input.tgz run_dir")
+        jobid = uv.run_job(j, nx=procs[0], ny=procs[1], nz=procs[2], jtype="queue", run_time="1:00:00")
