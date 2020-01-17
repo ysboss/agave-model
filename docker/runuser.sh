e9627dd9167d570fbcd69d4fd9ca090b870e9d6d
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-01-17 09:27:36 -0500

    New docker setup

--- docker/runuser.sh
+++ docker/runuser.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+cd /usr/local/python/JetLag
+git pull
+
+cd
+
+if [ -r .gitconfig ]
+then
+    git config --global user.email "sbrandt@cct.lsu.edu"
+    git config --global user.name "Steven Brandt"
+fi
 
 if [ ! -d agave-model ]
 then
   git clone https://github.com/ysboss/agave-model.git
-  git config --global user.email "syuan@lsu.edu"
-  git config --global user.name "Shuai Yuan"
+  git checkout newmodel
 fi
 cd agave-model
 
@@ -29,9 +38,9 @@ SECRET_TOKEN=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 echo
 echo "To access the notebook, open this file in a browser copy and paste this URL:"
 echo
-echo " http://localhost:8003/?token=${SECRET_TOKEN}"
+echo " http://localhost:${PORT}/?token=${SECRET_TOKEN}"
 echo
-jupyter notebook --ip=0.0.0.0 --port=8003 --no-browser --NotebookApp.token="${SECRET_TOKEN}"
+jupyter notebook --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token="${SECRET_TOKEN}"
 
 # One can also create a custom URL
 # jupyter notebook --ip=0.0.0.0 --port=8003 --no-browser --NotebookApp.custom_display_url="http://localhost:8003"
