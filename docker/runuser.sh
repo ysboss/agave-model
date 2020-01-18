f4c43bdbaec89255aa6610d79c9042c344a458a4
Steven Brandt <sbrandt@cct.lsu.edu>
2020-01-17 21:46:25 -0600

    Adding write_env.py

--- docker/runuser.sh
+++ docker/runuser.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-cd /usr/local/python/JetLag
-git pull
-
-cd
-
-if [ -r .gitconfig ]
-then
-    git config --global user.email "sbrandt@cct.lsu.edu"
-    git config --global user.name "Steven Brandt"
-fi
 
 if [ ! -d agave-model ]
 then
   git clone https://github.com/ysboss/agave-model.git
-  cd ~/agave-model
-  git checkout newmodel
+  git config --global user.email "syuan@lsu.edu"
+  git config --global user.name "Shuai Yuan"
 fi
-cd ~/agave-model
+cd agave-model
 
 sudo tzupdate
 
@@ -31,7 +21,7 @@ do
     if [ $? = 0 ]
     then
         # if it's not, then fetch the newest version from master.
-        git checkout origin/newmodel -- $i
+        git checkout origin/master -- $i
     fi
 done
 
@@ -39,9 +29,9 @@ SECRET_TOKEN=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 echo
 echo "To access the notebook, open this file in a browser copy and paste this URL:"
 echo
-echo " http://localhost:${PORT}/?token=${SECRET_TOKEN}"
+echo " http://localhost:8003/?token=${SECRET_TOKEN}"
 echo
-jupyter notebook --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token="${SECRET_TOKEN}"
+jupyter notebook --ip=0.0.0.0 --port=8003 --no-browser --NotebookApp.token="${SECRET_TOKEN}"
 
 # One can also create a custom URL
 # jupyter notebook --ip=0.0.0.0 --port=8003 --no-browser --NotebookApp.custom_display_url="http://localhost:8003"
